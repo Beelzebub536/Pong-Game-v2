460a9310e90cac93edb8bdba3d508ef6f69fd45d
Li <hells.gate536@gmail.com>
2024-05-12 19:05:26 +0600

    Fixed ceiling and floor collision.

--- src/Ball.java
+++ src/Ball.java
@@ -2,28 +2,28 @@ import java.util.Random;
 
 public class Ball {
     public static final int BALL_SIZE = 20;
-    private int ballX,ballY;
+    private int ballX, ballY;
     private final int BALL_SPEED = 2;
     private int ballXSpeed = BALL_SPEED, ballYSpeed = BALL_SPEED;
-    private Random random = new Random();
+    private final Random random = new Random();
 
-    public Ball(){
+    public Ball() {
         resetBall();
     }
 
-    public void resetBall(){
+    public void resetBall() {
         ballXSpeed = BALL_SPEED;
         ballYSpeed = BALL_SPEED;
         int halfW = GamePanel.WIDTH / 2;
         int halfH = GamePanel.HEIGHT / 2;
-        if(random.nextBoolean()) {
+        if (random.nextBoolean()) {
             ballXSpeed *= -1;
         }
-        if (random.nextBoolean()){
+        if (random.nextBoolean()) {
             ballYSpeed *= -1;
         }
-        ballX=random.nextInt((int) (halfW-halfW*0.2), (int) (halfW+halfW*0.2));
-        ballY=random.nextInt((int) (halfH-halfH*0.2), (int) (halfH+halfH*0.2));
+        ballX = random.nextInt((int) (halfW - halfW * 0.2), (int) (halfW + halfW * 0.2));
+        ballY = random.nextInt((int) (halfH - halfH * 0.2), (int) (halfH + halfH * 0.2));
     }
 
     public int getBallX() {
@@ -60,13 +60,21 @@ public class Ball {
 
     public void moveBall() {
         ballX += ballXSpeed;
-        ballY += ballYSpeed;
-        if (ballY <= GamePanel.CEILING || ballY >= GamePanel.HEIGHT - BALL_SIZE) {
+        if (ballY + ballYSpeed <= GamePanel.CEILING) {
             ballYSpeed = -ballYSpeed;
-            if (ballXSpeed >0)
+            ballY = ballYSpeed - ballY + 2*GamePanel.CEILING;
+            if (ballXSpeed > 0)
                 ballXSpeed++;
             else
                 ballXSpeed--;
-        }
+        } else if (ballY + ballYSpeed >= GamePanel.HEIGHT - BALL_SIZE) {
+            ballY = 2*(GamePanel.HEIGHT - BALL_SIZE) - ballYSpeed - ballY;
+            ballYSpeed = -ballYSpeed;
+            if (ballXSpeed > 0)
+                ballXSpeed++;
+            else
+                ballXSpeed--;
+        } else
+            ballY += ballYSpeed;
     }
 }
--- src/CheckCollision.java
+++ src/CheckCollision.java
@@ -27,32 +27,3 @@ public class CheckCollision {
             circle.moveBall();
     }
 }
-
-
-//import java.awt.*;
-//
-//public class CheckCollision {
-//    public void checkCollision(Player player1, Player player2, Ball circle) throws InterruptedException {
-//        Rectangle paddle1 = new Rectangle(0, player1.getPaddleY(), Player.PADDLE_WIDTH, Player.PADDLE_HEIGHT);
-//        Rectangle paddle2 = new Rectangle(GamePanel.WIDTH - Player.PADDLE_WIDTH, player2.getPaddleY(), Player.PADDLE_WIDTH, Player.PADDLE_HEIGHT);
-//
-//        if (circle.getBallX()+circle.getBallXSpeed() <= 0 || circle.getBallX()+circle.getBallXSpeed() >= GamePanel.WIDTH - Ball.BALL_SIZE) {
-//            int x = Ball.BALL_SIZE+1;
-//            int y = (int) (x * Math.tan(Math.atan2(circle.getBallYSpeed(), circle.getBallXSpeed())));
-//            Rectangle ball = new Rectangle(x, y, Ball.BALL_SIZE, Ball.BALL_SIZE);
-//            if (ball.intersects(paddle1) || ball.intersects(paddle2)) {
-//                circle.setBallXSpeed(-1*circle.getBallXSpeed());
-//                circle.setBallX(circle.getBallXSpeed()-circle.getBallX());
-//            }
-//        }
-//
-//        if (circle.getBallX() <= 0 || circle.getBallX() >= GamePanel.WIDTH - Ball.BALL_SIZE) {
-//            if (circle.getBallX() <= 0)
-//                player2.setScore(10 + player2.getScore());
-//            else
-//                player1.setScore(10 + player1.getScore());
-//            circle.resetBall();
-//            Thread.sleep(1000);
-//        }
-//    }
-//}
